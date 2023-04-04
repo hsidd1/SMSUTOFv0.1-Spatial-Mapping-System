3526f53dee489658ba13b94b606082ed2985a6c5
hsidd1 <hamzasiddiqui247@gmail.com>
2023-04-04 14:00:36 -0400

    add dependencies

--- src/SysTick.c
+++ src/SysTick.c
@@ -14,6 +14,7 @@
 // Copyright information is at the bottom of the file
 
 #include <stdint.h>
+#include "SysTick.h"
 #define NVIC_ST_CTRL_R          (*((volatile uint32_t *)0xE000E010))
 #define NVIC_ST_RELOAD_R        (*((volatile uint32_t *)0xE000E014))
 #define NVIC_ST_CURRENT_R       (*((volatile uint32_t *)0xE000E018))
