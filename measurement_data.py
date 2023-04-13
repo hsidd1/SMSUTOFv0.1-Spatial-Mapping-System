c3c0b4cefbea2f89fd934d59e77fd92a6e38d5fa
hsidd1 <hamzasiddiqui247@gmail.com>
2023-04-12 20:56:10 -0400

    modified iteration

--- measurement_data.py
+++ measurement_data.py
@@ -1,22 +1,10 @@
-# Hamza Siddiqui - 400407170 
-# Last Modified 2023-04-10
-'''
-Program to write measurement data from ToF to .xyz file
-Usage: After iputting number of increments when running this program, 
-       Run the respective Keil project utilizing I2C to interface with microcontroller and VL53L1X Time of Flight sensor
-       This program will then use UART communication to receive measurements and print to console and write to a .xyz file
-       Make sure to close any software using the same port for communication, such as RealTerm, before using
-       Modify relevant parameters based on C code configuration
-'''
-# Importing necessary packages 
 import serial
 import math
 
 # Modify to the correct UART port for your PC - this program uses COM4
 PORT = 'COM4'
-s = serial.Serial(PORT,115200) # 115200 is the baud rate
+s = serial.Serial(PORT, 115200) # 115200 is the baud rate
 
-s.open
 # Reset input and output buffers 
 s.reset_output_buffer()
 s.reset_input_buffer()
@@ -29,10 +17,11 @@ with open("pointdata.xyz", "w") as f:
     ROTATION = 32 # Modify to number of steps for next measurement taken
     TOTAL_STEPS = 512 # One rotation is 512 steps for 28BYJ-48 motor
     num_increments = int(input("Enter number of increments in measurement: ")) # How many displacement increments the program runs for 
-    for i in range(num_increments):
+    count = 0
+    while count < num_increments:
         # Format from UART - decode into string and remove special output characters
         data = s.readline().decode("utf-8")[0:-2]
-        if (data.isdigit()):
+        if data.isdigit():
             # Compute angle and coordinates
             angle = (steps/TOTAL_STEPS)*2*math.pi 
             r = int(data)
@@ -43,7 +32,9 @@ with open("pointdata.xyz", "w") as f:
             print(z)
             f.write(f'{x} {y} {z}\n')
             steps += ROTATION # For next measurement 
-        if (steps == 512): #reset number of steps after a full rotation is completed and increament x and count
+        if steps == 512:
+            # Reset number of steps after a full rotation is completed and increment x and count
             steps = 0
             x += STEP_INCREMENT
+            count += 1
         print(data)
